2d542af2c2b49179d3ebcdbb3649a0bef4a2740b
Neil Tate <57351607+Neil-Tate@users.noreply.github.com>
2020-03-12 11:53:49 +0200

    practice for form-data

---
+++ app.py
@@ -0,0 +1,42 @@
+# from flask import Flask, redirect, url_for
+# app = Flask(__name__)
+
+# @app.route('/admin')
+# def hello_admin():
+#    return 'Hello Admin'
+
+# @app.route('/guest/<guest>')
+# def hello_guest(guest):
+#    return 'Hello %s as Guest' % guest
+
+# @app.route('/user/<name>')
+# def hello_user(name):
+#    if name =='admin':
+#       return redirect(url_for('hello_admin'))
+#    else:
+#       return redirect(url_for('hello_guest',guest = name))
+
+# if __name__ == '__main__':
+#    app.run(debug = True)
+
+from flask import Flask, redirect, url_for, request, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return render_template('index.html')
+
+@app.route('/success/<name>')
+def success(name):
+    return 'Welcome %s' % name
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success',name = user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success',name = user))
+if __name__ == '__main__':
+    app.run(debug  = True)
